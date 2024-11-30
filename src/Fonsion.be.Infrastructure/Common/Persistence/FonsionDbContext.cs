069722290a28412575094a9a4e70103b87b3b6b5
brani <branislannjemec@gmail.com>
2024-11-30 23:09:28 +0100

    create DbContext

---
+++ src/Fonsion.be.Infrastructure/Common/Persistence/FonsionDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Fonsion.be.Application.Common.Interfaces;
+using Fonsion.be.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fonsion.be.Infrastructure.Common.Persistence;
+
+public class FonsionDbContext: DbContext, IUnitOfWork
+{
+
+    public FonsionDbContext(DbContextOptions options): base(options)
+    {
+        
+    }
+    
+    public DbSet<Room> Rooms { get; set; } = null!;
+    
+    
+    public async Task CommitChangesAsync()
+    {
+       await base.SaveChangesAsync();
+    }
+}
