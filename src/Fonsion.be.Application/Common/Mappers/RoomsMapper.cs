1460d001d6aa746011bcc40397eb5086221cad1c
brani <branislannjemec@gmail.com>
2024-11-30 23:10:20 +0100

    Create Room mapper

---
+++ src/Fonsion.be.Application/Common/Mappers/RoomsMapper.cs
@@ -0,0 +1,28 @@
+﻿using Fonsion.be.Contracts.Rooms;
+using Fonsion.be.Domain.Entities;
+
+namespace Fonsion.be.Application.Common.Mappers;
+
+public static class RoomsMapper
+{
+
+    public static Room FromRequestToModel(this CreateRoomRequest createRoomRequest)
+    {
+        return new Room()
+        {
+            Id = Guid.NewGuid(),
+            Name = createRoomRequest.Name,
+            Description = createRoomRequest.Description,
+            ImageUrl = createRoomRequest.ImageUrl,
+            Capacity = createRoomRequest.Capacity,
+            PricePerNight = createRoomRequest.pricePerNight
+
+        };
+    }
+
+    public static CreateRoomResponse FromModelToResponse(this Room room)
+    {
+        return new CreateRoomResponse(room.Name, room.Capacity, room.PricePerNight, room.Description, room.ImageUrl);
+    }
+    
+}
