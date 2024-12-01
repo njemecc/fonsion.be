21c899b2ee460a6b1d0425ef46336f1044abd32f
brani <branislannjemec@gmail.com>
2024-12-01 23:20:47 +0100

    create GetAllRoomsQuery and handler

---
+++ src/Fonsion.be.Application/Rooms/Queries/GetAllRooms/GetAllRoomsQuery.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using Fonsion.be.Contracts.Rooms;
+using MediatR;
+
+namespace Fonsion.be.Application.Rooms.Queries.GetAllRooms;
+
+public record GetAllRoomsQuery() : IRequest<ErrorOr<List<CreateRoomResponse>>>;
