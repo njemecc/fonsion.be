674e6be57f87e91c7c15990d89cea4cc888601a8
brani <branislannjemec@gmail.com>
2024-12-05 23:48:22 +0100

    inherit IdentityDbContext with his options

--- src/Fonsion.be.Infrastructure/Common/Persistence/FonsionDbContext.cs
+++ src/Fonsion.be.Infrastructure/Common/Persistence/FonsionDbContext.cs
@@ -1,17 +1,25 @@
 ﻿using Fonsion.be.Application.Common.Interfaces;
 using Fonsion.be.Domain.Entities;
+using Fonsion.be.Domain.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fonsion.be.Infrastructure.Common.Persistence;
 
-public class FonsionDbContext: DbContext, IUnitOfWork
+public class FonsionDbContext: IdentityDbContext<User,IdentityRole,string>, IUnitOfWork
 {
 
     public FonsionDbContext(DbContextOptions options): base(options)
     {
         
     }
-    
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
     public DbSet<Room> Rooms { get; set; } = null!;
     
     
