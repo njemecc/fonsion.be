af1459ac091c825d632d957e683c92cda59002c0
brani <branislannjemec@gmail.com>
2024-12-06 17:42:52 +0100

    Create Command for creating an user

---
+++ src/Fonsion.be.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using ErrorOr;
+using Fonsion.be.Application.Common.Dtos.Users;
+using Fonsion.be.Contracts.Rooms;
+using MediatR;
+
+namespace Fonsion.be.Application.Users.Commands.CreateUser;
+
+public record CreateUserCommand(CreateUserRequest CreateUserRequest) : IRequest<ErrorOr<CreateUserResponse>>;
