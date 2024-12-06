36e08b9e8c88deacb1f6be5c6b0c7b769d848621
brani <branislannjemec@gmail.com>
2024-12-06 17:43:15 +0100

    create Dtos for creating a user request and response

---
+++ src/Fonsion.be.Application/Common/Dtos/Users/CreateUserRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Fonsion.be.Application.Common.Dtos.Users;
+
+public record CreateUserRequest(string FirstName, string LastName, string Email, string Password);
---
+++ src/Fonsion.be.Application/Common/Dtos/Users/CreateUserResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace Fonsion.be.Application.Common.Dtos.Users;
+
+public record CreateUserResponse(string Email, string Token);
