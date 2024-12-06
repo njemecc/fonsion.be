563589c6847832616bce667bf6ba714ebbb9518b
brani <branislannjemec@gmail.com>
2024-12-06 17:44:47 +0100

    create a Service for creating a JWT token

---
+++ src/Fonsion.be.Application/Common/Services/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Fonsion.be.Application.Common.Interfaces;
+using Fonsion.be.Domain.Users;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Fonsion.be.Application.Common.Services;
+
+public class TokenService : ITokenService
+{
+    private readonly IConfiguration _config;
+    private readonly SymmetricSecurityKey _key;
+
+    public TokenService(IConfiguration config)
+    {
+        _config = config;
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:SecretKey"]));
+    }
+
+    public string CreateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName)
+        };
+
+        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddHours(1),
+            SigningCredentials = creds,
+            Issuer = _config["JWT:Issuer"],
+            Audience = _config["JWT:Audience"]
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
