290003efebaf566b0fe253d513d3353bb1a9e4f9
brani <branislannjemec@gmail.com>
2024-12-07 20:37:34 +0100

    create GuestCompanionRepository and its interface

---
+++ src/Fonsion.be.Application/Common/Interfaces/IGuestCompanionRepository.cs
@@ -0,0 +1,8 @@
+﻿using Fonsion.be.Domain.GuestCompanion;
+
+namespace Fonsion.be.Application.Common.Interfaces;
+
+public interface IGuestCompanionRepository
+{
+    Task AddGuestCompanionAsync(GuestCompanion guestCompanion);
+}
