d422ca3abc076c4f84fc61afd10b0fae1eb70150
brani <branislannjemec@gmail.com>
2024-12-07 20:39:58 +0100

    create Reservation Controller

---
+++ src/Fonsion.be.Api/Controllers/ReservationController.cs
@@ -0,0 +1,35 @@
+﻿using Fonsion.be.Application.Common.Dtos.Reservations;
+using Fonsion.be.Application.Reservations.Commands.CreateReservation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fonsion.be.Api.Controllers;
+
+
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ReservationController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ReservationController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    
+    [HttpPost]
+
+    public async Task<IActionResult> CreateReservation([FromBody] CreateReservationRequest request)
+    {
+        var command = new CreateReservationCommand(request);
+
+        var response = await _mediator.Send(command);
+
+        return Ok(response);
+
+
+    }
+    
+}
