d3448ca8b4a773fede9f7f816d919a3258c31595
brani <branislannjemec@gmail.com>
2024-12-07 20:38:26 +0100

    create PromoCodeRepository.cs

---
+++ src/Fonsion.be.Application/Common/Interfaces/IPromoCodeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Fonsion.be.Domain.PromoCodes;
+
+namespace Fonsion.be.Application.Common.Interfaces;
+
+public interface IPromoCodeRepository
+{
+    Task<PromoCode?> GetPromoCodeByIdAsync(Guid? promoCodeId);
+
+    Task AddPromoCodeAsync(PromoCode? promoCode);
+
+    Task UpdatePromoCodeAsync(PromoCode promoCode);
+}
