f703dfcdebaa41b6016a90e8b0716f431278872f
njemecc <branislannjemec@gmail.com>
2024-12-12 17:21:26 +0100

    create new migration after adding Image

---
+++ src/Fonsion.be.Infrastructure/Migrations/20241212154322_ImageEntity.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Fonsion.be.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9304ae8a-be01-472b-9bd3-2d439e5f02de");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a9b55b4b-140b-4e35-878c-46fc934ba875");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Rooms");
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Url = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RoomId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "325fb4d7-ba29-483d-b2fe-e41fe1d6e583", null, "Admin", "ADMIN" },
+                    { "59ad11c5-433a-4978-9513-24a59a2d1ead", null, "Guest", "GUEST" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "325fb4d7-ba29-483d-b2fe-e41fe1d6e583");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "59ad11c5-433a-4978-9513-24a59a2d1ead");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Rooms",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "9304ae8a-be01-472b-9bd3-2d439e5f02de", null, "Admin", "ADMIN" },
+                    { "a9b55b4b-140b-4e35-878c-46fc934ba875", null, "Guest", "GUEST" }
+                });
+        }
+    }
+}
