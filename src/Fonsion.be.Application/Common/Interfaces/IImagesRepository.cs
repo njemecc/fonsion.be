0b3bf25ed837c86fe3e7e10eb2734ae7ad1282e4
njemecc <branislannjemec@gmail.com>
2024-12-12 17:20:00 +0100

    Create AddImageAsync method

---
+++ src/Fonsion.be.Application/Common/Interfaces/IImagesRepository.cs
@@ -0,0 +1,8 @@
+﻿using Fonsion.be.Domain.Images;
+
+namespace Fonsion.be.Application.Common.Interfaces;
+
+public interface IImagesRepository
+{
+    Task AddImageAsync(Image image);
+}
---
+++ src/Fonsion.be.Infrastructure/Images/Persistance/ImagesRepository.cs
@@ -0,0 +1,20 @@
+﻿using Fonsion.be.Application.Common.Interfaces;
+using Fonsion.be.Domain.Images;
+using Fonsion.be.Infrastructure.Common.Persistence;
+
+namespace Fonsion.be.Infrastructure.Images.Persistance;
+
+public class ImagesRepository : IImagesRepository
+{
+    private readonly FonsionDbContext _dbContext;
+
+    public ImagesRepository(FonsionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddImageAsync(Image image)
+    {
+         await _dbContext.Images.AddAsync(image) ;
+    }
+}
