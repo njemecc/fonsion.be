21279a842414d2ef7c4a874592213e2c9bf041c8
njemecc <branislannjemec@gmail.com>
2024-12-12 17:19:22 +0100

    create Image for every image that room has

--- src/Fonsion.be.Application/Rooms/Commands/CreateRoom/CreateRoomCommandHandler.cs
+++ src/Fonsion.be.Application/Rooms/Commands/CreateRoom/CreateRoomCommandHandler.cs
@@ -2,6 +2,7 @@
 using Fonsion.be.Application.Common.Interfaces;
 using Fonsion.be.Application.Common.Mappers;
 using Fonsion.be.Contracts.Rooms;
+using Fonsion.be.Domain.Images;
 using MediatR;
 
 namespace Fonsion.be.Application.Rooms.Commands.CreateRoom;
@@ -11,11 +12,13 @@ public class CreateRoomCommandHandler:IRequestHandler<CreateRoomCommand,ErrorOr<
 
     private readonly IRoomsRepository _roomsRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IImagesRepository _imagesRepository;
 
-    public CreateRoomCommandHandler(IRoomsRepository roomsRepository, IUnitOfWork unitOfWork)
+    public CreateRoomCommandHandler(IRoomsRepository roomsRepository, IUnitOfWork unitOfWork, IImagesRepository imagesRepository)
     {
         _roomsRepository = roomsRepository;
         _unitOfWork = unitOfWork;
+        _imagesRepository = imagesRepository;
     }
 
     public async Task<ErrorOr<CreateRoomResponse>> Handle(CreateRoomCommand request, CancellationToken cancellationToken)
@@ -26,6 +29,18 @@ public class CreateRoomCommandHandler:IRequestHandler<CreateRoomCommand,ErrorOr<
         var createRoomResponse = room.FromModelToResponse();
         
         await _roomsRepository.AddRoomAsync(room);
+        
+        foreach (var imageUrl in request.CreateRoomRequest.ImageUrls.Where(iurl => iurl != null))
+        {
+            var Image = new Image
+            {
+                Url = imageUrl,
+                RoomId = room.Id
+            };
+
+            await _imagesRepository.AddImageAsync(Image);
+        }
+        
         //unit of work pattern:
         await _unitOfWork.CommitChangesAsync();
 
