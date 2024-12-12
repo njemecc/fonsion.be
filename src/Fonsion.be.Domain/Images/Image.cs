b31c06e02ec265d66a0749c73f47939e9f1080a2
njemecc <branislannjemec@gmail.com>
2024-12-12 17:19:38 +0100

    create Image

---
+++ src/Fonsion.be.Domain/Images/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace Fonsion.be.Domain.Images;
+
+public class Image
+{
+    public Guid Id { get; set; }
+    
+    public string Url { get; set; }
+
+    public Guid RoomId { get; set; }
+}
