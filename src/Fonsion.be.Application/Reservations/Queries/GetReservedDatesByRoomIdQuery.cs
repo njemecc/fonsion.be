82b3fa53392a7bb9f88d342e76f60a0fb756f601
njemecc <branislannjemec@gmail.com>
2024-12-25 10:51:32 +0100

    create GetReservedDatesByRoomIdQuery and its handler

---
+++ src/Fonsion.be.Application/Reservations/Queries/GetReservedDatesByRoomIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Fonsion.be.Application.Reservations.Queries;
+
+public record GetReservedDatesByRoomIdQuery(Guid roomId) : IRequest<ErrorOr<List<string>>>;
---
+++ src/Fonsion.be.Application/Reservations/Queries/GetReservedDatesByRoomIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+using Fonsion.be.Application.Common.Interfaces;
+using MediatR;
+
+namespace Fonsion.be.Application.Reservations.Queries;
+
+public class GetReservedDatesByRoomIdQueryHandler : IRequestHandler<GetReservedDatesByRoomIdQuery,ErrorOr<List<string>>>
+{
+    
+    private readonly IReservationsRepository _reservationsRepository;
+
+    public GetReservedDatesByRoomIdQueryHandler(IReservationsRepository reservationsRepository)
+    {
+        _reservationsRepository = reservationsRepository;
+    }
+
+
+    public async Task<ErrorOr<List<string>>> Handle(GetReservedDatesByRoomIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _reservationsRepository.GetReservedDatesByRoomIdAsync(request.roomId);
+    }
+}
