42e316d35687a8425a4a07b9f30cd9c9a2a6e71d
njemecc <branislannjemec@gmail.com>
2024-12-25 10:50:30 +0100

    new migrations

---
+++ src/Fonsion.be.Infrastructure/Migrations/20241213171000_addImagesToRoomEntity.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Fonsion.be.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addImagesToRoomEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "325fb4d7-ba29-483d-b2fe-e41fe1d6e583");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "59ad11c5-433a-4978-9513-24a59a2d1ead");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1af4d67d-bae2-4841-88f0-ed786d99f6d3", null, "Admin", "ADMIN" },
+                    { "c8398bf8-afd7-4629-811a-638ee69e31a6", null, "Guest", "GUEST" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_RoomId",
+                table: "Images",
+                column: "RoomId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Rooms_RoomId",
+                table: "Images",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Rooms_RoomId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_RoomId",
+                table: "Images");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1af4d67d-bae2-4841-88f0-ed786d99f6d3");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c8398bf8-afd7-4629-811a-638ee69e31a6");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "325fb4d7-ba29-483d-b2fe-e41fe1d6e583", null, "Admin", "ADMIN" },
+                    { "59ad11c5-433a-4978-9513-24a59a2d1ead", null, "Guest", "GUEST" }
+                });
+        }
+    }
+}
---
+++ src/Fonsion.be.Infrastructure/Migrations/20241224180747_addReservationsToRoomEntity.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Fonsion.be.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addReservationsToRoomEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1af4d67d-bae2-4841-88f0-ed786d99f6d3");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c8398bf8-afd7-4629-811a-638ee69e31a6");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "ee8f3a27-289c-4821-9a71-5f58a566df67", null, "Guest", "GUEST" },
+                    { "fbefa7f0-eac9-4073-8588-815b8ab3c19a", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ee8f3a27-289c-4821-9a71-5f58a566df67");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fbefa7f0-eac9-4073-8588-815b8ab3c19a");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1af4d67d-bae2-4841-88f0-ed786d99f6d3", null, "Admin", "ADMIN" },
+                    { "c8398bf8-afd7-4629-811a-638ee69e31a6", null, "Guest", "GUEST" }
+                });
+        }
+    }
+}
