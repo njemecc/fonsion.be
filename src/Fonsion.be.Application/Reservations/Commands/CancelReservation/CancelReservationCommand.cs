d624fc705f70b6d5ed01a345054ae0244a6009eb
njemecc <branislannjemec@gmail.com>
2025-01-08 23:25:44 +0100

    create CancelReservationCommand and Handler

---
+++ src/Fonsion.be.Application/Reservations/Commands/CancelReservation/CancelReservationCommand.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Fonsion.be.Application.Reservations.Commands.CancelReservation;
+
+public record CancelReservationCommand(Guid ReservationId) : IRequest<ErrorOr<string>>;
+    
