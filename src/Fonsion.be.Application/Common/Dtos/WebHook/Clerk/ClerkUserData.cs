807b342409d90f5789d1ee30d98ecbf8f99dce01
njemecc <branislannjemec@gmail.com>
2025-02-12 22:16:18 +0100

    create clerkWebHookEvent model dto

---
+++ src/Fonsion.be.Application/Common/Dtos/WebHook/Clerk/ClerkUserData.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+public class ClerkUserData
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("first_name")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("last_name")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("email_addresses")]
+    public List<EmailAddress> EmailAddresses { get; set; }
+}
+
+public class EmailAddress
+{
+    [JsonPropertyName("email_address")]
+    public string Email { get; set; }
+}
---
+++ src/Fonsion.be.Application/Common/Dtos/WebHook/Clerk/EventAttributes.cs
@@ -0,0 +1,7 @@
+﻿namespace Fonsion.be.Application.Common.Dtos.WebHook.Clerk;
+
+public class EventAttributes
+{
+    public string IpAddress { get; set; }
+    public string UserAgent { get; set; }
+}
